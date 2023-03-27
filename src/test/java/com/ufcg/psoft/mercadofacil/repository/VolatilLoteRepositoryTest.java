7f9bede034cf8888f73951fa63878f12a0444428
Miguel Rodrigues <miguel.dol.rodrigues@gmail.com>
2023-03-27 17:01:46 -0300

    Adding more tests to volatile repository

--- src/test/java/com/ufcg/psoft/mercadofacil/repository/VolatilLoteRepositoryTest.java
+++ src/test/java/com/ufcg/psoft/mercadofacil/repository/VolatilLoteRepositoryTest.java
@@ -26,6 +26,10 @@ class VolatilLoteRepositoryTest {
     Lote resultado;
     Produto produto;
 
+    Lote loteExtra;
+    Produto produtoExtra;
+
+
 
     @BeforeEach
     void setup() {
@@ -40,6 +44,19 @@ class VolatilLoteRepositoryTest {
                 .id(1L)
                 .numeroDeItens(100)
                 .produto(produto)
+                .build();
+
+        produtoExtra = Produto.builder()
+                .id(2L)
+                .nome("Produto Extra")
+                .codigoBarra("987654321")
+                .fabricante("Fabricante Extra")
+                .preco(125.36)
+                .build();
+        loteExtra = Lote.builder()
+                .id(2L)
+                .numeroDeItens(100)
+                .produto(produtoExtra)
                 .build();
     }
 
@@ -56,7 +73,6 @@ class VolatilLoteRepositoryTest {
     void salvarPrimeiroLote() {
         resultado = driver.save(lote);
 
-
         assertEquals(driver.findAll().size(),1);
         assertEquals(resultado.getId().longValue(), lote.getId().longValue());
         assertEquals(resultado.getProduto(), produto);
@@ -66,30 +82,94 @@ class VolatilLoteRepositoryTest {
     @Test
     @DisplayName("Adicionar o segundo Lote (ou posterior) no repositorio de dados")
     void salvarSegundoLoteOuPosterior() {
-        Produto produtoExtra = Produto.builder()
-                .id(2L)
-                .nome("Produto Extra")
-                .codigoBarra("987654321")
-                .fabricante("Fabricante Extra")
-                .preco(125.36)
-                .build();
-        Lote loteExtra = Lote.builder()
-                .id(2L)
-                .numeroDeItens(100)
-                .produto(produtoExtra)
-                .build();
+
         driver.save(lote);
-
-
         resultado = driver.save(loteExtra);
-
 
         assertEquals(driver.findAll().size(),2);
         assertEquals(resultado.getId().longValue(), loteExtra.getId().longValue());
         assertEquals(resultado.getProduto(), produtoExtra);
-
-
     }
 
+    @Test
+    @DisplayName("Dar update em lote inexistente")
+    void updateEmLoteInexistente() {
 
+        driver.save(lote);
+
+        resultado = driver.update(loteExtra);
+
+        assertEquals(resultado, null);
+        assertEquals(driver.findAll().size(),2);
+    }
+
+    @Test
+    @DisplayName("Delete de um lote")
+    void deleteDeUmLote() {
+        driver.save(lote);
+        driver.save(loteExtra);
+
+        driver.delete(lote);
+
+        assertEquals(driver.findAll().size(),1);
+    }
+
+    @Test
+    @DisplayName("Encontrar lote")
+    void findLote() {
+
+        driver.save(lote);
+        driver.save(loteExtra);
+
+        resultado = driver.find(lote.getId());
+
+        assertEquals(resultado, lote);
+    }
+
+    @Test
+    @DisplayName("Encontrar lote que não está no repositório")
+    void findLoteForaDoRepositorio() {
+
+        driver.save(lote);
+        driver.save(loteExtra);
+
+        resultado = driver.find(50L);
+
+        assertEquals(resultado, null);
+    }
+
+    @Test
+    @DisplayName("Deletando um lote")
+    void deletandoUmLote() {
+        driver.save(lote);
+        driver.save(loteExtra);
+
+        driver.delete(lote);
+
+        assertEquals(driver.findAll().size(), 1);
+        assertEquals(driver.find(loteExtra.getId()), loteExtra);
+    }
+
+    @Test
+    @DisplayName("Deletando tudo de uma vez")
+    void deletandoTudoDeUmaVez() {
+        driver.save(lote);
+        driver.save(loteExtra);
+
+        driver.deleteAll();
+
+        assertEquals(driver.findAll().size(), 0);
+    }
+
+    @Test
+    @DisplayName("Deletando tudo um por um")
+    void deletandoTudoUmPorUm() {
+        driver.save(lote);
+        driver.save(loteExtra);
+
+        driver.delete(lote);
+        driver.delete(loteExtra);
+
+        assertEquals(driver.findAll().size(), 0);
+    }
 }
